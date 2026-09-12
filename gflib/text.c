5e425206babb0bba3d2a5a79d5a4a6fceb0a963a
tejasmr <tejasmr@outlook.com>
2026-09-12 22:03:07 +0530

    Refactor window vertical scroll speeds to use named constants for text speed options

--- gflib/text.c
+++ gflib/text.c
@@ -73,7 +73,12 @@ static const u8 sDarkDownArrowTiles[] = INCBIN_U8("graphics/fonts/down_arrow_alt
 static const u8 sUnusedFRLGBlankedDownArrow[] = INCBIN_U8("graphics/fonts/unused_frlg_blanked_down_arrow.4bpp");
 static const u8 sUnusedFRLGDownArrow[] = INCBIN_U8("graphics/fonts/unused_frlg_down_arrow.4bpp");
 static const u8 sDownArrowYCoords[] = { 0, 1, 2, 1 };
-static const u8 sWindowVerticalScrollSpeeds[] = { 0x1, 0x2, 0x4, 0x4 };
+static const u8 sWindowVerticalScrollSpeeds[] = {
+    [OPTIONS_TEXT_SPEED_SLOW] = 1,
+    [OPTIONS_TEXT_SPEED_MID] = 2,
+    [OPTIONS_TEXT_SPEED_FAST] = 4,
+    [OPTIONS_TEXT_SPEED_INST] = 4,
+};
 
 static const struct GlyphWidthFunc sGlyphWidthFuncs[] =
 {
